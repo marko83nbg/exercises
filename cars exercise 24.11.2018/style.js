98dab8f7e0fcfaf0c95efb213ff416c584b7be5f
Marko <marko83nbg@gmail.com>
2018-11-24 15:05:39 +0100

    gotov

--- cars exercise 24.11.2018/style.js
+++ cars exercise 24.11.2018/style.js
@@ -1,5 +1,24 @@
-function test() {
-    
+var indeksSlike = 1;
+
+function prethodna() {
+    if ( indeksSlike == 1 ){
+        indeksSlike = 4;
+    } else {
+        indeksSlike--;
+    }
+    document.getElementById('car').src = 'img/car' + indeksSlike + '.jpg'; // jedna fotka je png
 }
+
+
+function sledeca() {
+    if (indeksSlike == 4) {
+        indeksSlike = 1;
+    } else {
+        indeksSlike++;
+    }
+    document.getElementById('car').src = 'img/car' + indeksSlike + '.jpg';
+}
+
+
 
 
